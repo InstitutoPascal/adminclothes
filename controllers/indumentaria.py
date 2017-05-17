c371ebb69d0f252485ddda4e47f58b4c1ab308ef
Ivan Zavala <ivanzavala231@gmail.com>
2017-05-17 18:43:30 -0300

    se agrego controlador indumentaria

---
+++ controllers/indumentaria.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# intente algo como
+def index(): return dict(message="hello from indumentaria.py")
+def remeras():
+    return dict(message="remeras")
+def camisas():
+    return dict(message="camisas")
+def jeans():
+    return dict(message="jeans")
+def bermudas():
+    return dict(message="bermudas")
+def abrigos():
+    return dict(message="abrigos")
